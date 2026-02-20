e046da444f3161a19652aa23833a4fe110179910
Aleksandra <olaa.stolarczuk@gmail.com>
2026-02-20 19:54:23 +0100

    Enable automatic payment methods

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,28 +1,31 @@
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST(req: Request) {
   const { plan } = await req.json()
 
   const priceMap: Record<string, string> = {
-  Podstawowy: 'price_1T2cwXBKaFMW1zBpFXvGBqF9',
-  Premium: 'price_1T2d0RBKaFMW1zBpFPzmYkzq',
-  'Pro+': 'price_1T2d2sBKaFMW1zBpq0nSyX9b',
+    Podstawowy: 'price_1T2cwXBKaFMW1zBpFXvGBqF9',
+    Premium: 'price_1T2d0RBKaFMW1zBpFPzmYkzq',
+    'Pro+': 'price_1T2d2sBKaFMW1zBpq0nSyX9b',
   }
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
     mode: 'payment',
+
+    automatic_payment_methods: {
+      enabled: true,
+    },
+
     line_items: [
       {
         price: priceMap[plan],
         quantity: 1,
       },
     ],
+
     success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success`,
     cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/pricing`,
   })
