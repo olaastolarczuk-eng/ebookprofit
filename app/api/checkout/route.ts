b214e4cd5f1c249c2200e18ef99a56cf57872b20
Aleksandra <olaa.stolarczuk@gmail.com>
2026-03-02 21:21:33 +0100

    Remove automatic_payment_methods

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-06-20' as Stripe.LatestApiVersion,
 })
 
 export async function POST(req: Request) {
