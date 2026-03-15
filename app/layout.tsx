30d5e70562532584d10e073c62f98ab9cb781df2
Aleksandra <olaa.stolarczuk@gmail.com>
2026-03-15 11:17:14 +0100

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import CookieBanner from '@/components/CookieBanner'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'EbookProfit – Twórz ebooki w 10 minut',
@@ -9,6 +10,7 @@ export const metadata = {
   metadataBase: new URL(
   process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
 ),
+
   openGraph: {
     title: 'EbookProfit',
     description:
@@ -41,9 +43,51 @@ export default function RootLayout({
   return (
     <html lang="pl">
       <body>
-        {children}
-        <CookieBanner />
-      </body>
+
+  {children}
+  <CookieBanner />
+
+  {/* Google Analytics */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-Y75HMSY65J"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Y75HMSY65J');
+    `}
+  </Script>
+
+ {/* Meta Pixel */}
+<Script id="facebook-pixel" strategy="afterInteractive">
+{`
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1371863008071204');
+fbq('track', 'PageView');
+`}
+</Script>
+
+<noscript>
+<img
+height="1"
+width="1"
+style={{ display: 'none' }}
+src="https://www.facebook.com/tr?id=1371863008071204&ev=PageView&noscript=1"
+/>
+</noscript>
+
+</body>
     </html>
   )
 }
